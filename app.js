14fc54483b00c34b18003b90d76eb6a96eac91cd
faris <faris@mail.com>
2021-12-17 16:27:46 +0500

    :sparkles: add mongodb

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const path = require('path');
 const logger = require('morgan');
 const express = require('express');
+const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 
 const indexRouter = require('./routes/index');
+const config = require('./config/config');
+
+/**  Connecting MongoDB. **/
+mongoose.connect(config.mongodb, (error) => {
+  if (error) console.log(error);
+});
 
 const app = express();
 
