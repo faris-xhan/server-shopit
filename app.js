4a091b1a16fb462bdf2fa0ca97a039f779bd9894
faris <faris@mail.com>
2021-12-17 16:15:01 +0500

    :tada: setup backend for

---
+++ app.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const logger = require('morgan');
+const express = require('express');
+const cookieParser = require('cookie-parser');
+
+const indexRouter = require('./routes/index');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+module.exports = app;
