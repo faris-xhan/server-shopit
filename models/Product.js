76c8df37588c9d341638091e18e3032ae8940f06
faris <faris@mail.com>
2021-12-17 16:46:05 +0500

    :sparkles: create post and get route for product

--- models/Product.js
+++ models/Product.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
 const ProductSchema = new mongoose.Schema({
+  isSale: { type: Boolean },
   oldPrice: { type: Number },
   title: { type: String, require: true },
   price: { type: String, require: true },
   quantity: { type: Number, default: 1 },
-  isSale: { type: Boolean, require: true },
   description: { type: String, require: true },
 });
 
--- routes/product.js
+++ routes/product.js
@@ -1,10 +1,33 @@
 const express = require('express');
 const router = express.Router();
+const Product = require('../models/Product');
 
-router.get('/', (req, res, next) => {
-  return res.json({
-    message: 'product',
-  });
+router.get('/', async (req, res, next) => {
+  try {
+    const products = await Product.find();
+    return res.json({
+      status: 'success',
+      data: products,
+      error: '',
+    });
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+router.post('/', async (req, res, next) => {
+  try {
+    const product = new Product(req.body);
+    return res.json({
+      status: 'success',
+      data: await product.save(),
+      error: '',
+    });
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
 });
 
 module.exports = router;
