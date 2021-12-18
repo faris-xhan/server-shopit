c25485e7eed8c253ce002c8304048bc645ce30db
faris <faris@mail.com>
2021-12-18 23:24:40 +0500

    :critical: fix product model

--- models/Product.js
+++ models/Product.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const ProductSchema = new mongoose.Schema({
   category: {type:String, },
   title: { type: String, require: true },
-  price: { type: String, require: true },
+  price: { type: Number,  require: true },
   quantity: { type: Number, default: 1 },
   description: { type: String, require: true },
 });
