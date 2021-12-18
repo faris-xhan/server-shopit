32aa19a07c67c895b8028d31e434e81f0b6fff03
faris <faris@mail.com>
2021-12-18 16:04:33 +0500

    :fire: add category field for product

--- models/Product.js
+++ models/Product.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const ProductSchema = new mongoose.Schema({
   isSale: { type: Boolean },
   oldPrice: { type: Number },
+  category: {type:String, },
   title: { type: String, require: true },
   price: { type: String, require: true },
   quantity: { type: Number, default: 1 },
